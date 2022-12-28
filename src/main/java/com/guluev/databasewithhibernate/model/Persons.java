4d1c7e5e4ecc593d8a5be098187d9309497ac888
Tural Guluev <tural-98@bk.ru>
2022-12-28 19:10:13 +0300

    update fields in the persons class

--- src/main/java/com/guluev/databasewithhibernate/model/Persons.java
+++ src/main/java/com/guluev/databasewithhibernate/model/Persons.java
@@ -2,14 +2,17 @@ package com.guluev.databasewithhibernate.model;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Persons {
     @EmbeddedId
     private PersonsPrimaryKey personsPrimaryKey;
-    private String name;
-    private String surname;
-    private int age;
     private String phone_number;
     private String city_of_living;
 
--- src/test/java/com/guluev/databasewithhibernate/DatabaseWithHibernateApplicationTests.java
+++ src/test/java/com/guluev/databasewithhibernate/DatabaseWithHibernateApplicationTests.java
@@ -1,13 +1,30 @@
 package com.guluev.databasewithhibernate;
 
+import com.guluev.databasewithhibernate.model.Persons;
+import com.guluev.databasewithhibernate.repository.DataBaseRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DatabaseWithHibernateApplicationTests {
 
+    @Autowired
+    private DataBaseRepository dataBaseRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @BeforeEach
+    public void initDatabase(){
+    }
+
     @Test
     void contextLoads() {
+        dataBaseRepository.getPersonsByCity("");
     }
 
 }
