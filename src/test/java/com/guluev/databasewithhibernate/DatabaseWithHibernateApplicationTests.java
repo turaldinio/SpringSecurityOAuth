9b20434cff7304f41f7b42bb4b6107740ffa3a2f
Tural Guluev <tural-98@bk.ru>
2022-12-28 19:47:53 +0300

    starting to write tests

--- src/test/java/com/guluev/databasewithhibernate/DatabaseWithHibernateApplicationTests.java
+++ src/test/java/com/guluev/databasewithhibernate/DatabaseWithHibernateApplicationTests.java
@@ -1,6 +1,7 @@
 package com.guluev.databasewithhibernate;
 
 import com.guluev.databasewithhibernate.model.Persons;
+import com.guluev.databasewithhibernate.model.PersonsPrimaryKey;
 import com.guluev.databasewithhibernate.repository.DataBaseRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -8,6 +9,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Random;
 
 @SpringBootTest
 class DatabaseWithHibernateApplicationTests {
@@ -19,7 +23,14 @@ class DatabaseWithHibernateApplicationTests {
     private EntityManager entityManager;
 
     @BeforeEach
-    public void initDatabase(){
+    public void initDatabase() {
+        List<String> nameList = List.of("James", "Niki", "Floria", "Georgiy", "Vladimir");
+        List<String> surName = List.of("Sergeev", "Romanov", "Zhukov");
+        Persons.builder().personsPrimaryKey(PersonsPrimaryKey.
+                builder().
+                name(nameList.get(new Random().nextInt(nameList.size()))).
+                surname(surName.get(new Random().nextInt(surName.size()))).
+                age(new Random().nextInt() * 78).build()).build();
     }
 
     @Test
