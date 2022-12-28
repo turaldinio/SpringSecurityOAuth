83ad7b80ca35aadab7bb7508bf9867f5712fb60b
Tural Guluev <tural-98@bk.ru>
2022-12-28 20:38:56 +0300

    update getPersonsByCity method

--- src/main/java/com/guluev/databasewithhibernate/repository/DataBaseRepository.java
+++ src/main/java/com/guluev/databasewithhibernate/repository/DataBaseRepository.java
@@ -3,8 +3,11 @@ package com.guluev.databasewithhibernate.repository;
 import com.guluev.databasewithhibernate.model.Persons;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class DataBaseRepository {
@@ -13,10 +16,10 @@ public class DataBaseRepository {
     private EntityManager entityManager;
 
     @Transactional
-    public void getPersonsByCity(String city) {
+    public List<Persons> getPersonsByCity(String city) {
         var request = entityManager.createQuery("select p from Persons p where p.city_of_living= :city ", Persons.class);
         request.setParameter("city", city);
-        request.getResultList().forEach(System.out::println);
+        return request.getResultList();
 
     }
 
