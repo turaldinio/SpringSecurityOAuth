9c73ac5e5f33dfda01a652bd48f05645147944f7
Tural Guluev <tural-98@bk.ru>
2022-12-29 22:16:31 +0300

    application is work

---
+++ target/classes/db.changelog/migrations/002-insert_in_persons.sql
@@ -0,0 +1,5 @@
+insert into persons (name, surname, age, phone_number, city_of_living)
+values ('James', 'Larikov', 21, '12936812', 'Moscow'),
+       ('Larisa', 'Ivanova', 25, '12936812', 'Moscow'),
+       ('Kirill', 'Simbiotov', 29, '12936812', 'Detroid'),
+       ('Piter', 'Parker', 24, '12936812', 'Lipetsk');
