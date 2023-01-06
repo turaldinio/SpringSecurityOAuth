92a1c18b2b9edb312e33f2249871af5da21b94fe
Tural Guluev <tural-98@bk.ru>
2023-01-06 18:24:13 +0300

    added the getAllByCityOfLiving in DataBaseRepository

--- src/main/java/com/guluev/databasewithhibernate/repository/DataBaseRepository.java
+++ src/main/java/com/guluev/databasewithhibernate/repository/DataBaseRepository.java
@@ -7,12 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DataBaseRepository extends JpaRepository<Persons, Integer> {
 
-//    @Transactional
-//    public List<Persons> getPersonsByCity(String city) {
-//        var request = entityManager.createQuery("select p from Persons p where p.cityOfLiving= :city ", Persons.class);
-//        request.setParameter("city", city);
-//        return request.getResultList();
-//
-//    }
+    Persons getAllByCityOfLiving(String cityName);
+
 
 }
