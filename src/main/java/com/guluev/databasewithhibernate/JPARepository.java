b206f4a832260a1a0ffaad1e606f99f42f00b5c8
Tural Guluev <tural-98@bk.ru>
2023-01-06 18:16:51 +0300

    rename main class

--- src/main/java/com/guluev/databasewithhibernate/JPARepository.java
+++ src/main/java/com/guluev/databasewithhibernate/JPARepository.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class DatabaseWithHibernateApplication {
+public class JPARepository {
 
     public static void main(String[] args) {
-        SpringApplication.run(DatabaseWithHibernateApplication.class, args);
+        SpringApplication.run(JPARepository.class, args);
 
     }
 }
