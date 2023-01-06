81f7ae80dd61cd904139c8b8abeb5e943c732e52
Tural Guluev <tural-98@bk.ru>
2023-01-06 19:03:53 +0300

    added the mockito in test class

--- src/test/java/com/guluev/databasewithhibernate/DatabaseWithHibernateApplicationTests.java
+++ src/test/java/com/guluev/databasewithhibernate/DatabaseWithHibernateApplicationTests.java
@@ -19,31 +19,31 @@ import java.util.stream.IntStream;
 @SpringBootTest
 class DatabaseWithHibernateApplicationTests {
 
-    @Autowired
-    private DataBaseRepository dataBaseRepository;
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    @Transactional
-    @Test
-    void contextLoads() {
-        List<String> nameList = List.of("James", "Niki", "Floria", "Georgiy", "Vladimir");
-        List<String> surName = List.of("Sergeev", "Romanov", "Zhukov");
-        List<String> city = List.of("Saint-Petersburg", "Moscow", "Krasnodar", "Omsk", "Tumen", "Tver");
-        IntStream.range(0, 15).forEach(x -> {
-            var person = Persons.builder().personsPrimaryKey(PersonsPrimaryKey.
-                            builder().
-                            name(nameList.get(new Random().nextInt(nameList.size()))).
-                            surname(surName.get(new Random().nextInt(surName.size()))).
-                            age(new Random().nextInt(89)).build())
-                    .phoneNumber("74928347291").
-                    cityOfLiving(city.get(new Random().nextInt(city.size())))
-                    .build();
-            entityManager.persist(person);
-        });
-
-        dataBaseRepository.getPersonsByCity("Saint-Petersburg");
-    }
+//    @Autowired
+//    private DataBaseRepository dataBaseRepository;
+//
+//    @PersistenceContext
+//    private EntityManager entityManager;
+//
+//    @Transactional
+//    @Test
+//    void contextLoads() {
+//        List<String> nameList = List.of("James", "Niki", "Floria", "Georgiy", "Vladimir");
+//        List<String> surName = List.of("Sergeev", "Romanov", "Zhukov");
+//        List<String> city = List.of("Saint-Petersburg", "Moscow", "Krasnodar", "Omsk", "Tumen", "Tver");
+//        IntStream.range(0, 15).forEach(x -> {
+//            var person = Persons.builder().personsPrimaryKey(PersonsPrimaryKey.
+//                            builder().
+//                            name(nameList.get(new Random().nextInt(nameList.size()))).
+//                            surname(surName.get(new Random().nextInt(surName.size()))).
+//                            age(new Random().nextInt(89)).build())
+//                    .phoneNumber("74928347291").
+//                    cityOfLiving(city.get(new Random().nextInt(city.size())))
+//                    .build();
+//            entityManager.persist(person);
+//        });
+//
+//        dataBaseRepository.getPersonsByCity("Saint-Petersburg");
+//    }
 
 }
