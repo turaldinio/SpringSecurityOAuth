03980f59abc38a40ad044ca35bf96e0f53aa9849
Tural <65041919+turaldinio@users.noreply.github.com>
2023-01-06 21:18:21 +0300

    Delete target directory

--- target/classes/db.changelog/migrations/001-create_persons.sql
+++
@@ -1,9 +0,0 @@
-create table persons
-(
-    name           char(20) not null,
-    surname        char(20) not null,
-    age            int      not null,
-    phone_number   char(15) not null,
-    city_of_living char(20) not null,
-    primary key (name,surname,age)
-)
