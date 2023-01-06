abf5b2bf36740006b64dd3ed743b82e7a42aa6c2
Tural Guluev <tural-98@bk.ru>
2023-01-06 21:25:08 +0300

    the application is work

--- src/main/java/com/guluev/databasewithhibernate/repository/DataBaseRepository.java
+++ src/main/java/com/guluev/databasewithhibernate/repository/DataBaseRepository.java
@@ -2,6 +2,7 @@ package com.guluev.databasewithhibernate.repository;
 
 import com.guluev.databasewithhibernate.model.Persons;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,10 +11,13 @@ import java.util.Optional;
 @Repository
 public interface DataBaseRepository extends JpaRepository<Persons, Integer> {
 
+    @Query("select p from Persons p where p.cityOfLiving= :cityName")
     List<Persons> getAllByCityOfLiving(String cityName);
 
+    @Query("select p from Persons p where p.personsPrimaryKey.age< :age order by p.personsPrimaryKey.age")
     List<Persons> getPersonsByPersonsPrimaryKey_AgeLessThanOrderByPersonsPrimaryKey(int age);
 
+    @Query("select p from Persons p where p.personsPrimaryKey.name= :name and p.personsPrimaryKey.surname= :surname")
     Optional<Persons> findByPersonsPrimaryKey_NameAndPersonsPrimaryKey_Surname(String name, String surname);
 
 }
