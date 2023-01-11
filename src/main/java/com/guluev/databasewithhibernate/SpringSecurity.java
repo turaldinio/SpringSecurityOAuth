f80c3555149f09a212c89e337b255927f3981a68
Tural Guluev <tural-98@bk.ru>
2023-01-11 13:53:37 +0300

    rename main class name

--- src/main/java/com/guluev/databasewithhibernate/SpringSecurity.java
+++ src/main/java/com/guluev/databasewithhibernate/SpringSecurity.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class JPARepository {
+public class SpringSecurity {
 
     public static void main(String[] args) {
-        SpringApplication.run(JPARepository.class, args);
+        SpringApplication.run(SpringSecurity.class, args);
 
     }
 }
