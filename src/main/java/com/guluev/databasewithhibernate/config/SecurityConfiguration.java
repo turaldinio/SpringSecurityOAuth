56ffffcbd088112d50354619a458b75211a523c8
Tural Guluev <tural-98@bk.ru>
2023-01-16 18:03:17 +0300

    rename config class

--- src/main/java/com/guluev/databasewithhibernate/config/SecurityConfiguration.java
+++ src/main/java/com/guluev/databasewithhibernate/config/SecurityConfiguration.java
@@ -2,10 +2,7 @@ package com.guluev.databasewithhibernate.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import java.util.List;
 
 @Configuration
-public class Configurator {
+public class SecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
